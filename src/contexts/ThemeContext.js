ab356bb007f671a0ec7f0e9f063bdd834cd9358a
Stephen Kaye <stephenkayebarcelona@gmail.com>
2021-01-25 16:47:01 +0100

    Add toggleTheme method to ThemeContext and use in Navbar

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -15,11 +15,17 @@ export class ThemeProvider extends Component {
     this.state = {
       isDarkMode: true
     }
+    this.toggleTheme = this.toggleTheme.bind(this);
   }
+
+  toggleTheme() {
+    this.setState({isDarkMode: !this.state.isDarkMode})
+  }
+
   render() {
     //VALUE IS KEY PROP. WILL BE PASSED DOWN TO ALL DESCENDENTAS. CAN ONLY BE ONE VALUE.
     return (
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
         {this.props.children}
       </ThemeContext.Provider>
     )
